fa7bac85d4682140b398672c9d00b54a3cb69c94
Haruka31Dev <u202412824@upc.edu.pe>
2024-05-07 18:07:54 -0500

    owo

---
+++ src/Models/PayrollDetail.java
@@ -0,0 +1,9 @@
+package Models;
+
+public class PayrollDetails  extends Payroll{
+    private String affiliationName;
+    private 
+    public PayrollDetails(Employee employee, Config config, String systemDate) {
+        super(employee, config, systemDate);
+    }
+}
